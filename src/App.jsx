5a32bcf3a71d8fa378e24883e0117ab4060df80d
Kionga Singano <zacariaskiongasingano@gmail.com>
2026-07-17 13:28:15 +0100

    Primeira versão Colégio Jerma

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+function App(){
+
+return(
+<div className="min-h-screen bg-blue-900 flex items-center justify-center">
+
+<div className="bg-white p-10 rounded-xl shadow-xl">
+
+<h1 className="text-4xl font-bold text-blue-900">
+Sistema de Matrículas Online
+</h1>
+
+<p className="mt-4 text-gray-600">
+Instituição de Ensino Médio
+</p>
+
+<button className="mt-6 bg-blue-900 text-white px-6 py-3 rounded-lg">
+Fazer Matrícula
+</button>
+
+</div>
+
+</div>
+)
+
+}
+
+export default App
